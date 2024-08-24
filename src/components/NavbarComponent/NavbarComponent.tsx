3b0162bf6c2808487ae0352c5a0f481c3fa98fd1
yloading <marvieyuki06@gmail.com>
2024-08-24 17:50:30 +0800

    changed nav bg color

--- src/components/NavbarComponent/NavbarComponent.tsx
+++ src/components/NavbarComponent/NavbarComponent.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const Navbar: React.FC = () => {
   return (
-    <nav className="bg-blue-600 p-4">
+    <nav className="bg-[#378CE7] p-4">
       <div className="container mx-auto flex justify-between items-center">
         <h1 className="text-white text-xl font-bold">MoviesApp</h1>
         <ul className="flex space-x-4">
