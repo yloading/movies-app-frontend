fad367b5bdf0ccd4716aefd97f39fca2b6282b6b
yloading <marvieyuki06@gmail.com>
2024-09-01 13:39:50 +0800

    modified about page

--- src/pages/AboutPage/constants.ts
+++ src/pages/AboutPage/constants.ts
@@ -2,18 +2,18 @@ export const ITEMS = [
   {
     title: "What We Do",
     content:
-      "Our application aggregates movie scores from three popular movie rating websites, providing you with a consolidated view of how each movie is rated across these sources.",
+      "This application aggregates movie scores from three popular movie rating websites, providing you with a consolidated view of how each movie is rated across these sources.",
   },
   {
     title: "How It Works",
-    content: `- Frontend: Built with React, and Tailwind as the styling library, our user interface is designed to be user-friendly, making it easy for you to view their ratings and details.
-               \n- Backend: Our backend is powered by Express and Node.js, handling data processing and server-side logic efficiently.
-               \n- TypeScript: We use TypeScript in both Frontend and Backend to ensure that our code is robust and maintainable, providing a better development experience and reducing the risk of errors.`,
+    content: `- Frontend: Built with React, and Tailwind as the styling library, the user interface is designed to be user-friendly, making it easy for you to view their ratings and details.
+               \n- Backend: Backend is powered by Express and Node.js, handling data processing and server-side logic efficiently.
+               \n- TypeScript: Used TypeScript in both Frontend and Backend to ensure that our code is robust and maintainable, providing a better development experience and reducing the risk of errors.`,
   },
   {
     title: "Data Source",
     content:
-      "Please note that our application does not access or use official APIs from the movie rating websites. Instead, we utilize available data sources and scraping techniques to retrieve movie scores. Please take note that this project is for personal use only.",
+      "Please note that the application does not access or use official APIs from the movie rating websites. Instead, I utilize available data sources and scraping techniques to retrieve movie scores. Please take note that this project is for personal use only.",
   },
   {
     title: "Author",
